1683ea57a3c342243133a65fdfcbc54fd1ecb404
Ole Petter Dahlmann <olepetter@dahlmann.no>
2016-09-06 14:08:03 +0200

    CustomAuthorizeAttribute

---
+++ MicroserviceAuthentication.SecondMicroService/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MicroserviceAuthentication.SecondMicroService.Controllers
+{
+    public class TestController : ApiController
+    {
+        [CustomAuthorize]
+        public string Get()
+        {
+            return "test";
+        }
+    }
+}
